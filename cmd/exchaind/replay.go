0d09321c7af96cce663b276c30462d9ab8f5c52a
chunfengSun <516108736@qq.com>
2022-02-10 11:23:43 +0800

    Update replay.go

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -316,7 +316,7 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 		block := originBlockStore.LoadBlock(height)
 		meta := originBlockStore.LoadBlockMeta(height)
 		blockExec.SetIsAsyncDeliverTx(viper.GetBool(sm.FlagParalleledTx))
-		fmt.Println("ready to replay", block.Height)
+		log.Println("ready to replay", block.Height)
 		state, _, err = blockExec.ApplyBlock(state, meta.BlockID, block)
 		panicError(err)
 		if needSaveBlock {
