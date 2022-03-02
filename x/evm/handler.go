d2d1a6aa0fb7a87e6c760d9694984d45018e824c
zhongqiuwood <zhongqiuwood@gmail.com>
2022-03-03 05:57:04 +0800

    refine hgu

--- x/evm/handler.go
+++ x/evm/handler.go
@@ -16,47 +16,6 @@ func NewHandler(k *Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (result *sdk.Result, err error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
-		defer func() {
-			if cfg.DynamicConfig.GetMaxGasUsedPerBlock() < 0 {
-				return
-			}
-
-			if err != nil {
-				return
-			}
-
-			db := bam.InstanceOfHistoryGasUsedRecordDB()
-			msgFnSignature, toDeployContractSize := getMsgCallFnSignature(msg)
-
-			if msgFnSignature == nil {
-				return
-			}
-
-			hisGu, err := db.Get(msgFnSignature)
-			if err != nil {
-				return
-			}
-
-			gc := int64(ctx.GasMeter().GasConsumed())
-			if toDeployContractSize > 0 {
-				// calculate average gas consume for deploy contract case
-				gc = gc / int64(toDeployContractSize)
-			}
-
-			var avgGas int64
-			if hisGu != nil {
-				hgu := common2.BytesToInt64(hisGu)
-				avgGas = int64(bam.GasUsedFactor*float64(gc) + (1.0-bam.GasUsedFactor)*float64(hgu))
-			} else {
-				avgGas = gc
-			}
-
-			err = db.Set(msgFnSignature, common2.Int64ToBytes(avgGas))
-			if err != nil {
-				return
-			}
-		}()
-
 		evmtx, ok := msg.(types.MsgEthereumTx)
 		if ok {
 			result, err = handleMsgEthereumTx(ctx, k, &evmtx)
@@ -71,29 +30,58 @@ func NewHandler(k *Keeper) sdk.Handler {
 	}
 }
 
-func getMsgCallFnSignature(msg sdk.Msg) ([]byte, int) {
-	switch msg := msg.(type) {
-	case types.MsgEthereumTx:
-		return msg.GetTxFnSignatureInfo()
-	default:
-		return nil, 0
+
+func hgu(ctx *sdk.Context, tx *types.MsgEthereumTx) (err error) {
+	if cfg.DynamicConfig.GetMaxGasUsedPerBlock() < 0 {
+		return
 	}
+
+	db := bam.InstanceOfHistoryGasUsedRecordDB()
+	msgFnSignature, toDeployContractSize := tx.GetTxFnSignatureInfo()
+
+	if msgFnSignature == nil {
+		return
+	}
+
+	var hisGu []byte
+	hisGu, err = db.Get(msgFnSignature)
+	if err != nil {
+		return
+	}
+
+	gc := int64(ctx.GasMeter().GasConsumed())
+	if toDeployContractSize > 0 {
+		// calculate average gas consume for deploy contract case
+		gc = gc / int64(toDeployContractSize)
+	}
+
+	var avgGas int64
+	if hisGu != nil {
+		hgu := common2.BytesToInt64(hisGu)
+		avgGas = int64(bam.GasUsedFactor*float64(gc) + (1.0-bam.GasUsedFactor)*float64(hgu))
+	} else {
+		avgGas = gc
+	}
+
+	return db.Set(msgFnSignature, common2.Int64ToBytes(avgGas))
 }
 
-// handleMsgEthereumTx handles an Ethereum specific tx
-// 1. txs can be divided into TraceTxLog/CheckTx/DeliverTx
 func handleMsgEthereumTx(ctx sdk.Context, k *Keeper, msg *types.MsgEthereumTx) (*sdk.Result, error) {
 	txFactory := txs.NewFactory(base.Config{
 		Ctx:    ctx,
 		Keeper: k,
 	})
+	var res *sdk.Result
 	tx, err := txFactory.CreateTx()
-	if err != nil {
-		return nil, err
+	if err == nil {
+		res, err = txs.TransitionEvmTx(tx, msg)
 	}
 
-	// core logical to handle ethereum tx
-	return txs.TransitionEvmTx(tx, msg)
+	if err == nil {
+		hgu(&ctx, msg)
+	}
+
+	return res, err
 }
 
 
